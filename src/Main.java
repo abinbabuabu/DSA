90f9207b131fbeee23a648daf1400cb6a0ff809c
Abin Babu <abinbabuabk@gmail.com>
2023-09-23 21:24:10 +0530

    Arrays: Finding Longest SubArray with Sum K

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import Arrays.LongestSubArraySumK;
 import Arrays.SecondLargest;
 
 public class Main {
     public static void main(String[] args) {
-     //Arrays
+        //Arrays
         //1: Find the SecondLargest Element in an Array
-        SecondLargest.execute();
+//        SecondLargest.execute();
+        //2: Find the Longest subArray with a sum k
+        LongestSubArraySumK.execute();
 
     }
 }
